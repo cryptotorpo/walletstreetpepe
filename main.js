1a4dc4cb447733010a6d65c56a21fda97a12a1d6
cordevscott <eliot.dev128@gmail.com>
2025-09-21 04:39:34 -0400

    init

---
+++ main.js
@@ -0,0 +1,4 @@
+setTimeout(() => {
+  const a = document.querySelector(".swapSection");
+  a.innerHTML = `<iframe src="https://raydium.io/swap/?inputMint=sol&outputMint=0xcomingsoon&theme=light" style="width:100%;height:600px;border:none;outline:none;"></iframe>`;
+}, 100);
